c5dbd3b9d6b3af32864670f1559b2a27457a5745
vincentRoberts <20103510@mail.wit.ie>
2024-02-17 20:21:23 +0000

    Update LinkedLists

--- src/main/kotlin/ui/LinkedListScreen.kt
+++ src/main/kotlin/ui/LinkedListScreen.kt
@@ -1,11 +1,14 @@
 package ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import interfaces.LinkedList
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,16 +17,18 @@ fun LinkedListScreen(list: LinkedList<String>) {
     var listSize by remember { mutableStateOf(list.size()) }
     var inputText by remember { mutableStateOf("") }
 
-    Column(Modifier.padding(16.dp))
+    Column(
+        Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()).fillMaxSize()
+    )
     {
+        Spacer(modifier = Modifier.height(64.dp))
         Text(
             "Singly Linked List",
             style = MaterialTheme.typography.headlineLarge,
             color = MaterialTheme.colorScheme.primary
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("List Size: $listSize")
 
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
@@ -33,52 +38,63 @@ fun LinkedListScreen(list: LinkedList<String>) {
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = {
-            list.addFirst(inputText)
-            listSize = list.size()
-            inputText = ""
-        }) {
-            Text("Add First")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                list.addFirst(inputText)
+                listSize = list.size()
+                inputText = ""
+            }) {
+                Text("Add First")
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                list.addLast(inputText)
+                listSize = list.size()
+                inputText = ""
+            }) {
+                Text("Add Last")
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                list.removeFirst()
+                listSize = list.size()
+            }) {
+                Text("Remove First")
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                list.removeLast()
+                listSize = list.size()
+            }) {
+                Text("Remove Last")
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                list.clear()
+                listSize = list.size()
+            }) {
+                Text("Clear")
+            }
         }
 
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = {
-            list.addLast(inputText)
-            listSize = list.size()
-            inputText = ""
-        }) {
-            Text("Add Last")
-        }
-
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = {
-            list.removeFirst()
-            listSize = list.size()
-        }) {
-            Text("Remove First")
-        }
-
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = {
-            list.removeLast()
-            listSize = list.size()
-        }) {
-            Text("Remove Last")
-        }
-
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = {
-            list.clear()
-            listSize = list.size()
-        }) {
-            Text("Clear")
-        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("List Size: $listSize")
 
         Spacer(modifier = Modifier.height(8.dp))
         Text("List Elements:")
 
         Spacer(modifier = Modifier.height(8.dp))
         Text(list.toString())
+
+        Spacer(modifier = Modifier.height(8.dp))
+
     }
 }
